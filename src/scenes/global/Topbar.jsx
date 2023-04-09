944c97df4f9c826d84320281942c4a73ab7f2a62
jelly <>
2023-04-09 23:44:14 +0800

    mock data & theme setting

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,14 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import inputBase from "@mui/material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+const TopBar = () => {
+  return <div>TopBar</div>;
+};
+export default TopBar;
