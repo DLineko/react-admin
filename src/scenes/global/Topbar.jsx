0a2d06b2f80afe1aa56026dc13ecd0b0e1422e65
jelly <>
2023-04-10 00:19:53 +0800

    fix bugs

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -9,6 +9,17 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 const TopBar = () => {
-  return <div>TopBar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  return;
+  <Box display="flex" justifyContent="space-between" p={2}>
+    <Box
+      display="flex"
+      backgroundColor={colors.primary[400]}
+      borderRadius="3px"
+    />
+    <IconButton></IconButton>
+  </Box>;
 };
 export default TopBar;
